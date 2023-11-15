58df61970fedd834080daf25cb6068b0f43c10cb
Emmanuel <nkongolo64@gmail.com>
2023-11-16 00:08:21 +0200

    finish the auth and add the organization plus its layout

---
+++ app/(platform)/(dashboard)/organization/[organizationId]/_components/org-control.tsx
@@ -0,0 +1,11 @@
+"use client"
+
+import { useEffect } from "react"
+import { useParams } from "next/navigation"
+import { useOrganizationList } from "@clerk/nextjs"
+
+export const OrgControl = () => {
+    const params = useParams()
+
+    return null
+}
