d22f4f9b1ebce59d367d1be2b049976e0e3ebf2c
Emmanuel <nkongolo64@gmail.com>
2023-11-18 23:55:46 +0200

    create prisma schema, mobile-sidebar, nav-item, page for the organization and settings and intall prisma client plus finaly add the db.ts in lib

--- app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,7 +1,15 @@
 const OrganizationIdPage = () => {
     return (
         <div>
-            Organization Page!
+           <form action="">
+            <input
+            id="title"
+            name="title"
+            required
+            placeholder="Enter a board title"
+            className="border-black border p-1"
+            />
+           </form>
         </div>
     )
 }
