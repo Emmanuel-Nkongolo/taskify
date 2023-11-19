6b05a92d8278cf35bf21c1ca8ce315ddd5a116ab
Emmanuel <nkongolo64@gmail.com>
2023-11-19 23:54:00 +0200

    create board ts, deleteboardts and some other form form-delete, form-input, form-button and a safe action

--- app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,15 +1,21 @@
-const OrganizationIdPage = () => {
+import { db } from "@/lib/db"
+import { Board } from "./board"
+import { Form } from "./form"
+
+const OrganizationIdPage = async () => {
+    const boards = await db.board.findMany()
     return (
-        <div>
-           <form action="">
-            <input
-            id="title"
-            name="title"
-            required
-            placeholder="Enter a board title"
-            className="border-black border p-1"
-            />
-           </form>
+        <div className="flex flex-col space-y-4">
+          <Form />
+           <div className="space-y-2">
+            {boards.map((board) => (
+                <Board 
+                key={board.id}
+                title={board.title}
+                id={board.id}
+                />
+            ))}
+           </div>
         </div>
     )
 }
