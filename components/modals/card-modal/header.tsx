d106e3c110f7294a37d5425bf6417643e2531b76
Emmanuel <nkongolo64@gmail.com>
2023-12-02 12:17:59 +0200

    add the queryClient, inputRef, onBlur and the onSubmit in the header

--- components/modals/card-modal/header.tsx
+++ components/modals/card-modal/header.tsx
@@ -1,11 +1,13 @@
 "use client"
 
-import { useState } from "react"
+import { ElementRef, useRef, useState } from "react"
 import { Layout } from "lucide-react"
+import { useParams } from "next/navigation"
+import { useQueryClient } from "@tanstack/react-query"
 
 import { CardWithList } from "@/types"
-import { FormInput } from "@/components/form/form-input"
 import { Skeleton } from "@/components/ui/skeleton"
+import { FormInput } from "@/components/form/form-input"
 
 interface HeaderProps {
     data: CardWithList
@@ -14,18 +16,37 @@ interface HeaderProps {
 export const Header = ({
     data
 }: HeaderProps) => {
+    const queryClient = useQueryClient()
+    const params = useParams()
+
+    const inputRef = useRef<ElementRef<"input">>(null)
+    
     const [title, setTitle] = useState(data.title)
+
+    const onBlur = () => {
+        inputRef.current?.form?.requestSubmit()
+    }
+
+    const onSubmit = (formData: FormData) => {
+        console.log(formData.get("title"))
+    }
+
     return (
         <div className="flex items-start gap-x-3 mb-6 w-full">
             <Layout className="h-5 w-5 mt-1 text-neutral-700" />
             <div className="w-full">
-                <form>
+                <form action={onSubmit}>
                     <FormInput
+                    ref={inputRef}
+                    onBlur={onBlur}
                     id="title"
                     defaultValue={title}
                     className="font-semibold text-xl px-1 text-neutral-700 bg-transparent border-transparent relative -left-1.5 w-[95%] focus-visible:bg-white focus-visible:border-input mb-0.5 truncate"
                     />
                 </form>
+                <p className="text-sm text-muted-foreground">
+                    in list <span className="underline">{data.list.title}</span>
+                </p>
             </div>
         </div>
     )
